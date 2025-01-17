6eda9372aba16573d90a2c663085e06b1b506c32
FilipLand <filip9038@gmail.com>
2025-01-17 08:56:02 +0100

    Anyadir utilidades

--- Java/Trabajo/src/Main.java
+++ Java/Trabajo/src/Main.java
@@ -1,5 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import Utilidades.Util;
+
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello and welcome!\n");
---
+++ Java/Trabajo/src/Utilidades/Util.java
@@ -0,0 +1,67 @@
+package Utilidades;
+
+public class Util {
+    String Marca;
+    String Modelo;
+    int Antiguedad;
+    int Caballos;
+    int revoluciones;
+    boolean Funciona;
+
+    public Util(String marca, String modelo, int antiguedad, int caballos, int revoluciones, boolean funciones) {
+        Marca = marca;
+        Modelo = modelo;
+        Antiguedad = antiguedad;
+        Caballos = caballos;
+        this.revoluciones = revoluciones;
+        Funciona = funciones;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String modelo) {
+        Modelo = modelo;
+    }
+
+    public int getAntiguedad() {
+        return Antiguedad;
+    }
+
+    public void setAntiguedad(int antiguedad) {
+        Antiguedad = antiguedad;
+    }
+
+    public int getCaballos() {
+        return Caballos;
+    }
+
+    public void setCaballos(int caballos) {
+        Caballos = caballos;
+    }
+
+    public int getRevoluciones() {
+        return revoluciones;
+    }
+
+    public void setRevoluciones(int revoluciones) {
+        this.revoluciones = revoluciones;
+    }
+
+    public boolean isFunciona() {
+        return Funciona;
+    }
+
+    public void setFunciona(boolean funciona) {
+        Funciona = funciona;
+    }
+}
