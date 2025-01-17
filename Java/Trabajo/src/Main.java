f94220ad802312156747545a401b5084e4177668
FilipLand <filip9038@gmail.com>
2025-01-17 08:47:16 +0100

    Primer commit del trabajo

---
+++ Java/Trabajo/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!\n");
+        System.out.println("Welcome to Java!");
+            for (int i = 0; i <= 1000; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
