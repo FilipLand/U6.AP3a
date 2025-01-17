ebb2a79bc074bfdb652fa06be343522d83ff4f9d
FilipLand <filip9038@gmail.com>
2025-01-17 09:31:36 +0100

    Añado los primeros objetos en Main

--- Java/Trabajo/src/Main.java
+++ Java/Trabajo/src/Main.java
@@ -1,4 +1,4 @@
-
+import Utilidades.Personaje;
 import Utilidades.Util;
 
 public class Main {
@@ -8,5 +8,7 @@ public class Main {
             for (int i = 0; i <= 1000; i++) {
             System.out.println("i = " + i);
         }
+            new Personaje("Mario", 70, "Tanque", 70, 20, 128, 80, 70, 250, 70, "Guante Magico", "Caja Loca");
+            new Util("Citroen","C4", 5,500, 25000, true);
     }
 }
