9b2fbf0cd6922353ef524a488855d9b7da0e80b1
FilipLand <filip9038@gmail.com>
2025-01-21 09:55:22 +0100

    añado cosas

--- Java/Trabajo/src/Main.java
+++ Java/Trabajo/src/Main.java
@@ -10,7 +10,14 @@ public class Main {
         }
             Personaje p1 = new Personaje("Mario", 70, "Tanque", 70, 20, 128, 80, 70, 250, 70, "Guante Magico", "Caja Loca");
             Util u1 = new Util("Citroen","C4", 5,500, 25000, true);
-            System.out.println(p1.toString());
-            System.out.println(u1.toString());
+            System.out.println(p1);
+            System.out.println(u1);
+            System.out.println(p1.getNombre());
+            int numeroAletorio = (int) (Math.random() * 200) +1;
+            for (int i = 0; i < numeroAletorio; i++) {
+                p1.setNivel(p1.getNivel()+1);
+            }
+            System.out.println(p1.getNivel());
+            System.gc();
     }
 }
